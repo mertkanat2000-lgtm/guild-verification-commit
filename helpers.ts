a1b1ab4c3ac538851a00b883d52841f050b71370
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:11:50 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:11:46.097Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T16:11:51.260Z
+Commit: Add multi-language support
