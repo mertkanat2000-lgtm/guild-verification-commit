1cc6c06cc5f10c11a1de1887af8d6a49945b472d
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:16:20 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:16:06.292Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T16:16:21.981Z
+Commit: Implement user profile customization
