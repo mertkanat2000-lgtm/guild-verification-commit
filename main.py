bbdf9c13089e0c6254905b5ba2f1e6d9d8e3eb56
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:20:23 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:20:08.828Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T16:20:24.788Z
+Commit: Fix broken links in documentation
