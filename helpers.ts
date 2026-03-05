4976cacf067039309fe53d323c249046181ba744
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:13:54 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:13:50.862Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T16:13:55.694Z
+Commit: Fix CSS styling bug on mobile devices
