1c9a7d23511a5fde7b60f695aa34afd6df0ecefe
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:52:27 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:52:20.870Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T15:52:28.510Z
+Commit: Patch security vulnerability in authentication
