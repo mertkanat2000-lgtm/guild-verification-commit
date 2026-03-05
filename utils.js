150ff1ab17530535df73bc71a91ca10c9931f278
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:26:59 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:26:57.096Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T16:27:00.811Z
+Commit: Patch API endpoint error handling
