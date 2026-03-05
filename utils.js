fbf7d7c6a3d60dc5e8120ba2d8fbead2b0927ab8
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:26:07 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:25:49.841Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T16:26:08.450Z
+Commit: Resolve timezone issues in date handling
