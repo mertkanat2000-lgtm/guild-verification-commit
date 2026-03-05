f7f83aea5a2bd3ba18687cc70562a021369a1c98
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:20:48 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:20:19.951Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T16:20:49.509Z
+Commit: Resolve null pointer exception in user service
