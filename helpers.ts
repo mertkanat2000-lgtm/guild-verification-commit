b59d3abd4a92c7c40feea878c293d949955d53a3
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:14:38 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:14:29.683Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T16:14:39.949Z
+Commit: Add dark mode support
