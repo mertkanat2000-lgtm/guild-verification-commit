935cd892278b40b2a3f34ee6a95de4bc6deeedb3
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:11:19 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:09:55.179Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T16:11:20.910Z
+Commit: Introduce real-time notifications
