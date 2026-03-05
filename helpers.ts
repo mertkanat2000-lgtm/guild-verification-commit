29cda0cbfe048d7b788eab2e1fc9d223119b982c
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 17:59:06 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:59:05.801Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T14:59:07.196Z
+Commit: Fix race condition in async operations
