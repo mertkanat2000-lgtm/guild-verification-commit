3fddf01837d95055a11a7a4289d7c46e42804822
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:24:14 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:24:07.297Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T16:24:15.720Z
+Commit: Introduce real-time notifications
