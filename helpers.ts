c78d3938749e961a03f38777d879715177e5c315
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:45:00 +0300

    Add export functionality for reports

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:44:31.337Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T15:45:02.141Z
+Commit: Add export functionality for reports
