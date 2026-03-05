35503967a708f8b5b03680da4d45f5653c89bdb0
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:52:38 +0300

    Resolve timezone issues in date handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:52:18.498Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T15:52:39.429Z
+Commit: Resolve timezone issues in date handling
