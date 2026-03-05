b8527545e72f1da6521daefe0aeca9a615b39772
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:52:56 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:52:56.073Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T15:52:57.596Z
+Commit: Introduce real-time notifications
