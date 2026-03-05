c92bdcaaeeaf364e454f172c417383c65467a144
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:14:18 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:13:55.694Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T16:14:19.577Z
+Commit: Patch security vulnerability in authentication
