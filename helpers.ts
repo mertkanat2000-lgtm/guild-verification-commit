91725ec5aec4feef2456710cf3de100ede041ff6
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:20:32 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:20:27.213Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T16:20:33.202Z
+Commit: Add dark mode support
