5d7ee8cab0630fdcdbf5ff1ec0e280be685fd79c
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:27:12 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:26:18.926Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T16:27:13.509Z
+Commit: Fix CSS styling bug on mobile devices
