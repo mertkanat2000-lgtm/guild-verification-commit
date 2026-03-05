7c7acf22c57a0d01e00b0c7c1a74b65fe01bc707
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:22:49 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:22:36.836Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T16:22:51.112Z
+Commit: Introduce real-time notifications
