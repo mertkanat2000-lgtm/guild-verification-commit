9ceb24a7cb52df5fb73889902fd06576e710dc55
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:14:03 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:14:02.387Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T16:14:04.588Z
+Commit: Patch API endpoint error handling
