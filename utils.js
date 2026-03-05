e0b19d81194dbe183d62d88cdab5cfdb99d22dee
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:09:22 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:09:12.480Z
-Commit: Add dark mode support
+Timestamp: 2026-03-05T16:09:23.367Z
+Commit: Resolve null pointer exception in user service
