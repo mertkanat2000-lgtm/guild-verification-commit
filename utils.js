dedb1f8136cf733bae114aa27978b861d5c77a1e
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:27:05 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:27:00.811Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T16:27:06.406Z
+Commit: Fix CSS styling bug on mobile devices
