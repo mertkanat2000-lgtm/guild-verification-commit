8da83784a23d7ff4a94728a335e2dd8e69ae9457
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:42:01 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:41:55.639Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T15:42:02.801Z
+Commit: Add export functionality for reports
