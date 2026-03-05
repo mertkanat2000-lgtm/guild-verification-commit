b08c45d08c182018f6a2fe531ded46d2024c0c58
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:20:18 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:20:05.238Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T16:20:19.951Z
+Commit: Fix broken links in documentation
