df2a81943a7346c14437b5852bb9d108a13f8f0c
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:10:12 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:10:09.584Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T16:10:13.987Z
+Commit: Resolve timezone issues in date handling
