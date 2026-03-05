6a8e3fe6a62e773e76cacfc2aaa08d4a7f1e28be
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:50:31 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:50:12.568Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T15:50:33.199Z
+Commit: Introduce real-time notifications
