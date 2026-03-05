361b834a88b8d88dcb1726027900dfa06bedab0f
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:14:52 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:14:51.841Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T16:14:54.042Z
+Commit: Fix race condition in async operations
