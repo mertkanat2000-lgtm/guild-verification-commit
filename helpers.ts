607aed983f42069c1f2da3000364cf5200d5130a
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:10:11 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:10:01.910Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T16:10:12.670Z
+Commit: Fix memory leak in event handlers
