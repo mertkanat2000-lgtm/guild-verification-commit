28438363a3b7f873d048a62d9dc08ead1248af6c
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:52:19 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:52:13.964Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T15:52:20.870Z
+Commit: Add multi-language support
