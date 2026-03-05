e95cb7f392cbb6cbc4c159ba33522df0a1a79a0a
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:16:37 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:16:27.105Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T16:16:38.982Z
+Commit: Introduce real-time notifications
