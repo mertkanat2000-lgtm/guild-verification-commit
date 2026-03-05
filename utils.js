89a9d385691c12aa2b3f2571c026b21913f4d661
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:15:16 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:15:16.572Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T16:15:18.055Z
+Commit: Patch API endpoint error handling
