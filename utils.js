493df73794e49f149ebbfd3ec94462191fe2c1a9
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:15:15 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:15:12.173Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T16:15:16.572Z
+Commit: Patch security vulnerability in authentication
