4bb5d12ccca9fcbb98f7319b711dd604b35462dc
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:12:23 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:12:01.737Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T16:12:24.614Z
+Commit: Correct typo in configuration file
