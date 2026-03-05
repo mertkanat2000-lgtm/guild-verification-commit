e631eda56cb4b0fe19b71994e607fdd051d52cb9
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:12:49 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:12:49.709Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T16:12:50.789Z
+Commit: Introduce real-time notifications
