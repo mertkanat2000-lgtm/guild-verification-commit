5136750eff49dd3ac5d4cb46066f41a7c38dc126
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:15:34 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:15:26.106Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T16:15:36.156Z
+Commit: Resolve null pointer exception in user service
