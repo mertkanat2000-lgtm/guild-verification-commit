e0db213a5a21baa655a1dda7cf8259bc9798788e
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:51:32 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:51:30.512Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T15:51:33.286Z
+Commit: Correct typo in configuration file
