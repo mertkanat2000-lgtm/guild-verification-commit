71d2c7b4c9ee4287ec6d5831925607b74d5d0f2c
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:52:35 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:52:26.823Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T15:52:36.867Z
+Commit: Fix broken links in documentation
