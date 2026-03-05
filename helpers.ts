5327a953f8ac0c1c527991448107b3e133e2ad23
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 17:57:31 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T14:56:51.777Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T14:57:32.345Z
+Commit: Patch API endpoint error handling
