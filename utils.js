8e90e707725d342120a11b9aef0b5dc60570faf4
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:40:49 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:40:33.931Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T15:40:50.624Z
+Commit: Fix CSS styling bug on mobile devices
