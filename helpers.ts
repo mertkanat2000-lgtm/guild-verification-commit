0032cc41d5e19d39314835a23926193f48b7bbcf
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:45:53 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:45:32.577Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T15:45:54.321Z
+Commit: Fix broken links in documentation
