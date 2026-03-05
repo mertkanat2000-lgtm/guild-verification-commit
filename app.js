6a4e076a9d65e215ee20f89171c8f0174b1419d0
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:40:45 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:40:31.172Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T15:40:46.941Z
+Commit: Fix CSS styling bug on mobile devices
