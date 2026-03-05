9c57e32d582f195ede03e16b6904d50df27e76c6
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:39:29 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:39:10.639Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T15:39:30.314Z
+Commit: Resolve timezone issues in date handling
