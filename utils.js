1f2e3db5de6db9eac7bded00ecfd215a10ad4fcb
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:08:21 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:08:15.541Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T16:08:23.037Z
+Commit: Patch security vulnerability in authentication
