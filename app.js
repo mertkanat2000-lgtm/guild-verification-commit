bd1a8f7c3512c00e21aa3398b2137020744d479e
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:10:32 +0300

    Add export functionality for reports

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:10:21.801Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T16:10:33.516Z
+Commit: Add export functionality for reports
