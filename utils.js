3fff5c4b3e5e26d8d978952d83e8054db8bb1f73
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:48:31 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:48:30.084Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T15:48:33.220Z
+Commit: Add export functionality for reports
