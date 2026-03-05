1aee274405f8d5a6f87eff9bbcf3a3bd7997dd19
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:24:36 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:24:21.563Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T16:24:38.085Z
+Commit: Introduce real-time notifications
