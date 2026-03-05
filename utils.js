b98a2692a990a0a0d85fc4ec6f217e2576f0ead6
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:19:32 +0300

    Implement user profile customization

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:19:31.634Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T16:19:34.096Z
+Commit: Implement user profile customization
