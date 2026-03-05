32289e78c8947b8d4b5fd2f70728ffdc859639d5
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:14:50 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:14:34.992Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T16:14:51.841Z
+Commit: Fix broken links in documentation
