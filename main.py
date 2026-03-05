be53a0250d4f65237aacf3763ae19f61c5288aa0
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:11:26 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:11:20.910Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T16:11:27.370Z
+Commit: Resolve null pointer exception in user service
