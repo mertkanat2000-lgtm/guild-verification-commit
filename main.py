f4c41b8cb10159fe530f73995a53f84bffc46c96
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:52:48 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:52:31.349Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T15:52:50.095Z
+Commit: Patch API endpoint error handling
