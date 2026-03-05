91c05da489cbf2368fdd649d7920d629d39e5cfd
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:15:29 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:15:11.063Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T16:15:30.983Z
+Commit: Fix broken links in documentation
