40d486d1f4b85e548b97ee339542ccf53ca65a48
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:15:52 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:15:40.090Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T16:15:54.111Z
+Commit: Fix broken links in documentation
