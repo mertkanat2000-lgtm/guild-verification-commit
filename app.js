fe96e61175d6cb08bdf46897da838d90d3b47581
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:38:34 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:18:53.452Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T15:38:35.467Z
+Commit: Fix CSS styling bug on mobile devices
