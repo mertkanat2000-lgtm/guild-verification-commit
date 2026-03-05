81abffad806f911edaeebe7b15942529d21d065d
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:25:51 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:25:27.523Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T16:25:52.656Z
+Commit: Patch API endpoint error handling
