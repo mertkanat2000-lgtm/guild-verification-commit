bb7c86a0863473ace7bda0453b11caf889c6c766
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:52:14 +0300

    Fix database connection timeout

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:51:29.517Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T15:52:15.482Z
+Commit: Fix database connection timeout
