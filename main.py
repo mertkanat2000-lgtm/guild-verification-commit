21bbc025b71ed7e4ec31b5027ee4512bda7bbd95
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:16:11 +0300

    Patch security vulnerability in authentication

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:15:54.111Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T16:16:12.736Z
+Commit: Patch security vulnerability in authentication
