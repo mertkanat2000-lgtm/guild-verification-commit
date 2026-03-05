fa462ee0da3f24931341e302fdb0d417aa9061b2
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:17:08 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:16:21.981Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T16:17:09.429Z
+Commit: Fix broken links in documentation
