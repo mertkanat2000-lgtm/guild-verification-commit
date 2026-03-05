471d88be4f0cf496289ffebc448d54e9521aa6d6
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:15:24 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:14:47.100Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T16:15:26.106Z
+Commit: Fix broken links in documentation
