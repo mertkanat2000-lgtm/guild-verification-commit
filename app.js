17eb32e368b9d6398922e7f7eaffa3e19ce4b560
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:14:57 +0300

    Introduce real-time notifications

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:14:44.741Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T16:14:58.567Z
+Commit: Introduce real-time notifications
