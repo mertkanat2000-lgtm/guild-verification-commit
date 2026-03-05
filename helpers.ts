cb284ff0bcf6538c978cbfb775d669c4f5bdad7e
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:21:59 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:21:42.986Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T16:22:00.281Z
+Commit: Correct typo in configuration file
