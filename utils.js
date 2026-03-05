44b01b72ff2c8ce8a45639dcd64d99c0b7467a26
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:48:28 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:48:28.073Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T15:48:30.084Z
+Commit: Patch security vulnerability in authentication
