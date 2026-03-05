c6f57b1a84ea9b4f698ac568185d3c37a76ebf1c
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 18:44:40 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T15:44:29.361Z
-Commit: Add dark mode support
+Timestamp: 2026-03-05T15:44:42.133Z
+Commit: Patch security vulnerability in authentication
