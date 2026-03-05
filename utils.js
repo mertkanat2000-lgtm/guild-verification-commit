33d0f853a3c582fdc5b98e8e92ea39183d18b130
mertkanat2000-lgtm <mertkanat2000@gmail.com>
2026-03-05 19:09:38 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T16:09:23.367Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T16:09:39.992Z
+Commit: Fix race condition in async operations
